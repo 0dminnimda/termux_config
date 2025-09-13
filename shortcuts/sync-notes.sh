8591b19a2654f0bac784cca1c3e81c874449b0c0
0dminnimda <0dminnimda@gmail.com>
2025-09-13 20:08:01 +0400

    Remove shortcuts

--- shortcuts/sync-notes.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-# This script is executed by Termux:Widget for a one-tap sync.
-
-# Change to the notes repository directory
-cd /sdcard/Notes || exit
-
-# Execute the sync script
-bash git-sync.sh
